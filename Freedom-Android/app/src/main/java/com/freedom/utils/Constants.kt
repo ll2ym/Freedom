5919dbf747501c9a0989fe1b4e18868130042aef
ll2ym <gearmasters@gmail.com>
2025-08-08 15:47:43 -0400

    Fix: Implement invite code logic and connect Android app to server

--- Freedom-Android/app/src/main/java/com/freedom/utils/Constants.kt
+++ Freedom-Android/app/src/main/java/com/freedom/utils/Constants.kt
@@ -1,50 +1,7 @@
 package com.freedom.utils
 
 object Constants {
-
-    // Server API base URL
     const val BASE_URL = "http://10.0.2.2:8000/"
-
-    // WebSocket endpoints
-    const val CHAT_SOCKET_URL = "ws://10.0.2.2:8000/chat/ws"
-    const val CALL_SOCKET_URL = "ws://10.0.2.2:8000/calls/ws"
-
-    // Encryption
-    const val ENCRYPTION_ENABLED = true
-
-    // Signal Protocol settings
-    const val IDENTITY_KEY_ALIAS = "identity_key_pair"
-    const val PRE_KEY_COUNT = 100
-    const val PRE_KEY_BATCH_SIZE = 20
-
-    // SecurePrefs keys
-    const val PREF_AUTH_TOKEN = "auth_token"
-    const val PREF_USERNAME = "username"
-    const val PREF_SALT = "salt"
-    const val PREF_REGISTRATION_ID = "registration_id"
-    const val PREF_IDENTITY_KEY_PAIR = "identity_key_pair"
-
-    // Database
-    const val DATABASE_NAME = "freedom_secure.db"
-
-    // Intent extras
-    const val EXTRA_USERNAME = "extra_username"
-    const val EXTRA_GROUP_ID = "extra_group_id"
-
-    // Group limitations
-    const val MAX_GROUP_MEMBERS = 40
-
-    // UI/UX
-    const val DEFAULT_AVATAR_URL = "https://yourcdn.com/default_avatar.png"
-    const val APP_NAME = "Freedom"
-    const val APP_SLOGAN = "It is human right for secure communications!"
-
-    // WebRTC
-    const val STUN_SERVER = "stun:stun.l.google.com:19302"
-    const val TURN_SERVER = "turn:your-turn-server.com"
-    const val TURN_USERNAME = "freedom"
-    const val TURN_PASSWORD = "secureTurnPassword"
-
-    // Logging
-    const val LOG_TAG = "FreedomApp"
 }
+
+
